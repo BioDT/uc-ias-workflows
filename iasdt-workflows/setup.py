92daa341cee7f123188c291220a2ca3cf940147a
Taimur Khan <59065240+thisistaimur@users.noreply.github.com>
2023-02-14 12:55:09 +0100

    source

---
+++ iasdt-workflows/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data workflows for the IAS-DT, as part of BioDT.',
+    author='Taimur Khan, Helmholtz-UFZ',
+    license='MIT',
+)
