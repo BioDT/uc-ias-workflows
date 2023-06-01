e855cc1ccf843f5a615dc54136337377e0bf3b7c
Taimur Khan <59065240+thisistaimur@users.noreply.github.com>
2023-06-01 14:01:37 +0200

    feedbackloop for CHELSA added

--- workflows/dodo.py
+++ workflows/dodo.py
@@ -1,11 +1,20 @@
-from data.chelsa import download_data, geotiff_to_netcdf
+from data.chelsa import download_data, geotiff_to_netcdf, feedback
 
+
+def task_feedback():
+    """Feedbackloop CHELSA """
+    return {
+        "actions": [feedback()]
+    }
 
 def task_download_chelsa():
     """Download CHELSA data"""
     return {
         "actions": [download_data("../references/chelsa/test.txt", "../datasets/raw/chelsa")]
     }
+
+
+#TODO: Compare the raw datasets to the previos dataset to check for WHAT has changed
 
 def task_process_chelsa():
     """Process CHELSA .tif files to NetCDF4"""
