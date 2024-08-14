66f09852a7eb2df44ab8053b325b5b75c29fd67b
Taimur Khan <taimur.khan@ufz.de>
2024-08-14 15:59:26 +0300

    CORINE scrits added

---
+++ references/LUMI_Check_GPU.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+
+import tensorflow as tf
+print(tf.config.list_physical_devices("GPU"))
+
+tf.config.list_physical_devices("GPU")
---
+++ workflows/process/corine.R
@@ -0,0 +1,9 @@
+require(IASDT.R)
+
+
+IASDT.R::InfoChunk(
+  Message = "++++ PROCESSING CORINE LAND COVER DATA",
+  Rep = 2, Char = "+", CharReps = 70, Extra2 = 2)
+
+IASDT.R::CLC_Process(
+	EnvFile = "/pfs/lustrep4/scratch/project_465000915/khantaim/iasdt-workflows/.env", FromHPC = TRUE, MinLandPerc = 15, PlotCLC = TRUE)
